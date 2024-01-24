392b258cf198d4b6cf7aaaaf056674108fa313fe
QuantumCat957 <QuantumCat957@github.com>
2024-01-24 22:48:53 +0800

    add favorite & settings & nav

--- payload/src/resources/nav.rs
+++ payload/src/resources/nav.rs
@@ -2,11 +2,11 @@ use chrono::prelude::*;
 
 #[derive(serde::Deserialize, serde::Serialize, PartialEq, Debug, Default)]
 pub struct Nav {
-    pub id: i32,
+    pub id: u32,
     pub r#type: String,
-    pub type_id: i32,
-    pub user_id: i32,
-    pub sort: i32,
+    pub type_id: u32,
+    pub user_id: u32,
+    pub sort: u32,
     pub created_at: DateTime<Utc>,
     pub updated_at: Option<DateTime<Utc>>,
 }
