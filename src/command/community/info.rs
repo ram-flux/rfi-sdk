c408ffe12c763a68a0ce386e6c4336212a653ac6
QuantumCat957 <QuantumCat957@github.com>
2024-01-28 05:55:38 +0800

    community_list community_detail create_community done, need test

--- src/command/community/info.rs
+++ src/command/community/info.rs
@@ -1,86 +1,141 @@
+#[cfg(not(feature = "mock"))]
+use crate::sqlite_operator::Query as _;
+#[cfg(not(feature = "mock"))]
+use resource::Action as _;
+
+/// 社区列表
 pub async fn community_list(
     user_id: u32,
-) -> crate::response::Response<Vec<payload::resources::community::Community>> {
+) -> crate::response::Response<
+    crate::sqlite_operator::QueryResult<payload::resources::community::Community>,
+> {
     #[cfg(feature = "mock")]
     {
         let list = vec![
             payload::resources::community::Community {
-                id: 123,
                 father_id: None,
                 user_id: 6546,
                 name: "test".to_string(),
                 status: 1,
-                created_at: payload::utils::time::now(),
-                updated_at: Some(payload::utils::time::now()),
                 ..Default::default()
             },
             payload::resources::community::Community {
-                id: 124,
                 father_id: Some(123),
                 user_id: 5435,
                 name: "test2".to_string(),
                 passwd: Some("asdasd".to_string()),
                 pinned: true,
                 status: 2,
-                created_at: payload::utils::time::now(),
-                updated_at: Some(payload::utils::time::now()),
                 ..Default::default()
             },
         ];
-        return Ok(list).into();
+        return Ok(crate::sqlite_operator::QueryResult::Vec(list)).into();
     }
     #[cfg(not(feature = "mock"))]
-    todo!()
+    payload::resources::community::Community::query(
+        async move |user_pool, pub_pool| {
+        let sql =
+            "SELECT id, father_id, user_id, name, bio, passwd, announcement, pinned, status, created_at, 
+            updated_at FROM community 
+            WHERE user_id =$1;";
+        sqlx::query_as::<sqlx::Sqlite, _>(sql)
+            .bind(user_id)
+            .fetch_all(user_pool.as_ref())
+            .await
+            .map(Into::into)
+    })
+    .await
+    .map_err(|e| crate::Error::BadRequest(
+        crate::CommunityError::DatabaseError(e).into()
+    ))
+    .into()
 }
 
+/// 社区详情
 pub async fn community_detail(
     user_id: u32,
-) -> crate::response::Response<payload::resources::community::Community> {
+) -> crate::response::Response<
+    crate::sqlite_operator::QueryResult<payload::resources::community::Community>,
+> {
     #[cfg(feature = "mock")]
     {
-        let list = payload::resources::community::Community {
-            id: 124,
+        let comm = payload::resources::community::Community {
             father_id: Some(123),
             user_id: 5435,
             name: "test2".to_string(),
             passwd: Some("asdasd".to_string()),
             pinned: true,
             status: 2,
-            created_at: payload::utils::time::now(),
-            updated_at: Some(payload::utils::time::now()),
+            // created_at: payload::utils::time::now(),
+            // updated_at: Some(payload::utils::time::now()),
             ..Default::default()
         };
-        return Ok(list).into();
+        return Ok(crate::sqlite_operator::QueryResult::One(comm)).into();
     }
     #[cfg(not(feature = "mock"))]
-    todo!()
+    {
+        payload::resources::community::Community::query(async move |user_pool, pub_pool| {
+            let sql =
+                "SELECT id, father_id, user_id, name, bio, passwd, announcement, pinned, status FROM community 
+                WHERE user_id =$1;";
+            sqlx::query_as::<sqlx::Sqlite, payload::resources::community::Community>(sql)
+                .bind(user_id)
+                .fetch_one(user_pool.as_ref())
+                .await
+                .map(Into::into)
+        })
+        .await
+        .map_err(|e| crate::Error::BadRequest(
+            crate::InitDatabaseError::DatabaseError(e).into()
+        )).into()
+    }
 }
 
+/// 创建社区
 pub async fn create_community(
     user_id: u32,
     father_id: Option<u32>,
     bio: String,
     name: String,
+    announcement: Option<String>,
+    pinned: bool,
     status: u8,
     passwd: Option<String>,
 ) -> crate::response::Response<u32> {
-    let community = payload::resources::community::Community {
-        id: 12324,
-        father_id,
-        user_id,
-        name,
-        passwd,
-        status,
-        created_at: payload::utils::time::now(),
-        updated_at: Some(payload::utils::time::now()),
-        ..Default::default()
-    };
     #[cfg(feature = "mock")]
-    return Ok(community.id).into();
+    return Ok(3434).into();
     #[cfg(not(feature = "mock"))]
-    todo!()
+    {
+        let community = payload::resources::community::Community::new(
+            father_id,
+            user_id,
+            name,
+            bio,
+            passwd,
+            announcement,
+            pinned,
+            status,
+        );
+        let id = payload::utils::gen_id();
+        let community_action = resource::GeneralAction::Upsert {
+            id: Some(id),
+            resource: community,
+        };
+
+        let community_resource = crate::resources::Resources::Community(resource::Command::new(
+            user_id as i64,
+            community_action,
+            "UpsertCommunity".to_string(),
+        ));
+
+        let pool = crate::db::USER_SQLITE_POOL.read().await;
+        let pool = pool.get_pool().unwrap();
+        let _ = community_resource.execute(pool.as_ref()).await;
+        Ok(id).into()
+    }
 }
 
+/// 更新社区
 pub async fn update_community(
     community_id: u32,
     name: String,
@@ -96,6 +151,7 @@ pub async fn update_community(
     todo!()
 }
 
+/// 删除社区
 pub async fn del_community(community_id: u32) -> crate::response::Response<()> {
     #[cfg(feature = "mock")]
     return Ok(()).into();
