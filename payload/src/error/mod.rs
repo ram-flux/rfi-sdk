e6c8ca8eae045f7ae0c52d98f317d5536e23d11c
QuantumCat957 <QuantumCat957@github.com>
2024-01-28 05:47:19 +0800

    payload add error

---
+++ payload/src/error/mod.rs
@@ -0,0 +1,7 @@
+pub mod snowflake;
+
+#[derive(serde::Serialize, thiserror::Error, Debug, Clone)]
+pub enum PayloadError {
+    #[error("Snowflake error: {0}")]
+    Snowflake(#[from] snowflake::SnowflakeError),
+}
