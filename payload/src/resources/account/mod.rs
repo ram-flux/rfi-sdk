3db8dcb8fbf639909637066225479450250fd0fb
QuantumCat957 <QuantumCat957@github.com>
2024-01-28 05:52:29 +0800

    use resource macro

--- payload/src/resources/account/mod.rs
+++ payload/src/resources/account/mod.rs
@@ -1,9 +1,26 @@
-use chrono::prelude::*;
+// use chrono::prelude::*;
 
 pub mod avatar;
 pub mod community;
+pub mod elf;
 
-#[derive(serde::Deserialize, serde::Serialize, PartialEq, Debug, Default)]
+use resource::{GenResourceID, Resource};
+use sqlx::Sqlite;
+#[derive(
+    serde::Deserialize,
+    serde::Serialize,
+    PartialEq,
+    Debug,
+    resource::resource_macros::Resource,
+    Default,
+)]
+#[resource(
+    schema_name = "public",
+    pg_table_name = "account",
+    sqlite_table_name = "account",
+    primary_key = "id:u32",
+    constraint = "im_account_id_idx"
+)]
 pub struct Account {
     pub user_id: u32,
     pub public_key: String,
@@ -12,6 +29,40 @@ pub struct Account {
     pub name: String,
     pub avatar: String,
     pub bio: String,
-    pub created_at: DateTime<Utc>,
-    pub updated_at: Option<DateTime<Utc>>,
+    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+impl Account {
+    pub fn new(
+        user_id: u32,
+        public_key: String,
+        account: String,
+        gender: u8,
+        name: String,
+        avatar: String,
+        bio: String,
+    ) -> Self {
+        Self {
+            user_id,
+            public_key,
+            account,
+            gender,
+            name,
+            avatar,
+            bio,
+            created_at: crate::utils::time::now(),
+            updated_at: Some(crate::utils::time::now()),
+        }
+    }
+}
+
+impl resource::GenResourceID for Account {
+    type Target = u32;
+
+    async fn gen_id() -> Result<Self::Target, resource::Error> {
+        let mut id_worker = crate::utils::snowflake::SnowflakeIdWorkerInner::new(1, 1).unwrap();
+        let id1 = id_worker.next_id().unwrap();
+        Ok(id1 as u32)
+    }
 }
