fd61b49c16231656eab43c69c87dc6efefa76ae8
QuantumCat957 <QuantumCat957@github.com>
2024-01-30 09:21:10 +0800

    use im_codec

--- payload/src/resources/message/mod.rs
+++ payload/src/resources/message/mod.rs
@@ -1,10 +1,25 @@
 pub mod block;
 
 use chrono::prelude::*;
-
-#[derive(serde::Deserialize, serde::Serialize, PartialEq, Debug, Default)]
+use resource::{GenResourceID, Resource};
+use sqlx::Sqlite;
+#[derive(
+    serde::Deserialize,
+    serde::Serialize,
+    PartialEq,
+    Debug,
+    resource::resource_macros::Resource,
+    Default,
+    sqlx::FromRow,
+)]
+#[resource(
+    schema_name = "im",
+    pg_table_name = "message",
+    sqlite_table_name = "message",
+    primary_key = "id:u32",
+    constraint = "im_message_id_idx"
+)]
 pub struct Message {
-    pub id: u32,
     // text/文本/1 emoti/表情/2 file/文件/3 image/图片/5 video/视频/6 audio/音频/7 帖子/post/8 精灵/elf/9
     pub mode: u8,
     pub from_id: u32,
@@ -19,4 +34,46 @@ pub struct Message {
     pub accept_time: DateTime<Utc>,
     pub created_at: DateTime<Utc>,
     pub updated_at: Option<DateTime<Utc>>,
+}
+
+impl Message {
+    pub fn new(
+        mode: u8,
+        from_id: u32,
+        user_id: u32,
+        chat_id: u32,
+        chat_type: u8,
+        datas: String,
+        has_read: u8,
+        msg_error: u8,
+        status: u8,
+        send_time: DateTime<Utc>,
+        accept_time: DateTime<Utc>,
+    ) -> Self {
+        Self {
+            mode,
+            from_id,
+            user_id,
+            chat_id,
+            chat_type,
+            datas,
+            has_read,
+            msg_error,
+            send_time,
+            accept_time,
+            status,
+            created_at: crate::utils::time::now(),
+            updated_at: Some(crate::utils::time::now()),
+        }
+    }
+}
+
+impl resource::GenResourceID for Message {
+    type Target = u32;
+
+    async fn gen_id() -> Result<Self::Target, resource::Error> {
+        let mut id_worker = crate::utils::snowflake::SnowflakeIdWorkerInner::new(1, 1).unwrap();
+        let id1 = id_worker.next_id().unwrap();
+        Ok(id1 as u32)
+    }
 }
--- src/command/message.rs
+++ src/command/message.rs
@@ -1,10 +1,66 @@
+use std::{future::Future, pin::Pin};
+
+use im_codec::Protocol;
+
+// pub async fn init_tcp() {
+//     im_codec::Handler::handle_handshake(stream)
+// }
+
 pub async fn push_msg(
     content: String,
     mode: u8,
     from_id: i32,
     user_id: i32,
     chat_type: u8,
+    endpoint: String,
 ) -> crate::response::Response<()> {
+    let mut worker = payload::utils::worker();
+    let trace_id = worker.next_id().unwrap();
+
+    #[cfg(feature = "mock")]
+    let mut handler = {
+        let (sink, stream) = tokio::sync::mpsc::unbounded_channel();
+        let stream = tokio_stream::wrappers::UnboundedReceiverStream::new(stream);
+
+        let handler = im_codec::Handler::handshake(sink, stream, Protocol::heartbeat(trace_id))
+            .await
+            .unwrap();
+        handler
+    };
+
+    #[cfg(not(feature = "mock"))]
+    let mut handler = {
+        let handler =
+            im_codec::Handler::handshake(endpoint.parse().unwrap(), Protocol::heartbeat(trace_id))
+                .await
+                .unwrap();
+        handler
+    };
+
+    let sink = handler.sink();
+    let _ = sink.send(Protocol::heartbeat(worker.next_id().unwrap()));
+
+    tokio::spawn(async move {
+        let mop = |a: &[[u8; 32]],
+                   b: &[u8]|
+         -> Pin<Box<dyn Future<Output = Result<(), im_codec::Error>> + Send>> {
+            // TODO: 处理message
+            Box::pin(async move { Ok::<(), im_codec::Error>(()) })
+        };
+        let aop = |trace_id: u64,
+                   header: im_codec::ResponseHeader|
+         -> Pin<Box<dyn Future<Output = Result<(), im_codec::Error>> + Send>> {
+            // TODO: 处理response
+            Box::pin(async move { Ok::<(), im_codec::Error>(()) })
+        };
+        if let Err(e) = handler
+            .handle_request(|a, b| Box::pin(mop(a, b)), |a, b| Box::pin(aop(a, b)))
+            .await
+        {
+            tracing::error!("[handle_request] error: {e}");
+        };
+    });
+
     let message = payload::resources::message::Message {
         datas: content,
         ..Default::default()
@@ -66,3 +122,36 @@ pub async fn pin_msg(message_id: u32) -> crate::response::Response<()> {
     #[cfg(not(feature = "mock"))]
     todo!()
 }
+
+// struct Test {}
+
+// impl Test {
+//     pub async fn handle_request<MF, AF, U>(
+//         &mut self,
+//         message_op: MF,
+//         resp_op: AF,
+//     ) -> Result<(), crate::Error>
+//     where
+//         U: std::future::Future<Output = Result<(), crate::Error>>,
+//         MF: Fn(i8, i8) -> U,
+//         AF: Fn(u64 /* trace */, u64) -> U,
+//     {
+//         message_op(1, 1).await?;
+//         resp_op(1, 1).await?;
+//         Ok(())
+//     }
+// }
+
+// async fn a() {
+//     let op = |a: i8, b: i8| -> Pin<Box<dyn Future<Output = Result<(), crate::Error>>>> {
+//         Box::pin(async move { Ok::<(), crate::Error>(()) })
+//     };
+//     let aop = |a: u64, b: u64| -> Pin<Box<dyn Future<Output = Result<(), crate::Error>>>> {
+//         Box::pin(async move { Ok::<(), crate::Error>(()) })
+//     };
+
+//     let mut t = Test {};
+//     // t.handle_request(op, aop).await;
+//     t.handle_request(|a, b| Box::pin(op(a, b)), |a, b| Box::pin(aop(a, b)))
+//         .await;
+// }
