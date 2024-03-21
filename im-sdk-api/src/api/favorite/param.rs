258ad14baf81aaabf802c6a3b5bad19231a4ee7f
QuantumCat957 <QuantumCat957@github.com>
2024-03-22 02:39:50 +0800

    update favorite

---
+++ im-sdk-api/src/api/favorite/param.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, serde::Serialize)]
+pub struct AddFavoriteRes {
+    pub(crate) favorite_id: u32,
+}
+
+impl AddFavoriteRes {
+    pub async fn add_favorite(content: String) -> Result<Self, crate::Error> {
+        let favorite_id = crate::handler::favorite::add_favorite(content).await?;
+        Ok(Self { favorite_id })
+    }
+}
