34489814d183a4d99f2b9fb15193eddbbfe3a3a8
QuantumCat957 <QuantumCat957@github.com>
2024-03-22 02:37:45 +0800

    update elf

---
+++ im-sdk-api/src/api/elf/param.rs
@@ -0,0 +1,15 @@
+#[derive(Debug, serde::Serialize)]
+pub struct CreateElfRes {
+    pub(crate) elf_id: u32,
+}
+
+impl CreateElfRes {
+    pub async fn create_elf(
+        r#type: u8,
+        name: String,
+        avatar: String,
+    ) -> Result<Self, crate::Error> {
+        let elf_id = crate::handler::elf::create_elf(r#type, name, avatar).await?;
+        Ok(Self { elf_id })
+    }
+}
